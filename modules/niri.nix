17fe09b6db7a638a195905f684146e070744fb63
Joshua <joshua@example.com>
2026-05-17 00:20:26 +0000

    move greetd into niri aspect

--- modules/niri.nix
+++ modules/niri.nix
@@ -1,23 +1,33 @@
 {
-  den.aspects.niri.nixos =
-    { pkgs, ... }:
-    {
-      programs.niri.enable = true;
+  den.aspects.niri = {
+    nixos =
+      { pkgs, ... }:
+      {
+        programs.niri.enable = true;
 
-      environment.systemPackages = with pkgs; [
-        xwayland-satellite
-      ];
-
-      xdg.portal = {
-        enable = true;
-        extraPortals = with pkgs; [
-          xdg-desktop-portal-gnome
-          xdg-desktop-portal-gtk
+        environment.systemPackages = with pkgs; [
+          xwayland-satellite
         ];
+
+        xdg.portal = {
+          enable = true;
+          extraPortals = with pkgs; [
+            xdg-desktop-portal-gnome
+            xdg-desktop-portal-gtk
+          ];
+        };
+
+        security.polkit.enable = true;
+
+        services.gnome.gcr-ssh-agent.enable = false;
       };
 
-      security.polkit.enable = true;
-
-      services.gnome.gcr-ssh-agent.enable = false;
+    provides.joshua.nixos.services.greetd = {
+      enable = true;
+      settings.default_session = {
+        command = "niri-session";
+        user = "joshua";
+      };
     };
+  };
 }
--- modules/services.nix
+++ modules/services.nix
@@ -6,13 +6,5 @@
     services.tuned.enable = true;
     services.upower.enable = true;
     services.mullvad-vpn.enable = true;
-
-    services.greetd = {
-      enable = true;
-      settings.default_session = {
-        command = "niri-session";
-        user = "joshua";
-      };
-    };
   };
 }
