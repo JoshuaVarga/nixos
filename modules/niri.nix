4c949842b0eaa9171c992fed1c35d7c17f6746f4
Joshua <joshua@example.com>
2026-05-17 10:58:01 -0400

    move greetd back to host-scope nixos block

--- modules/niri.nix
+++ modules/niri.nix
@@ -1,33 +1,31 @@
 {
-  den.aspects.niri = {
-    nixos =
-      { pkgs, ... }:
-      {
-        programs.niri.enable = true;
+  den.aspects.niri.nixos =
+    { pkgs, ... }:
+    {
+      programs.niri.enable = true;
 
-        environment.systemPackages = with pkgs; [
-          xwayland-satellite
+      environment.systemPackages = with pkgs; [
+        xwayland-satellite
+      ];
+
+      xdg.portal = {
+        enable = true;
+        extraPortals = with pkgs; [
+          xdg-desktop-portal-gnome
+          xdg-desktop-portal-gtk
         ];
-
-        xdg.portal = {
-          enable = true;
-          extraPortals = with pkgs; [
-            xdg-desktop-portal-gnome
-            xdg-desktop-portal-gtk
-          ];
-        };
-
-        security.polkit.enable = true;
-
-        services.gnome.gcr-ssh-agent.enable = false;
       };
 
-    provides.joshua.nixos.services.greetd = {
-      enable = true;
-      settings.default_session = {
-        command = "niri-session";
-        user = "joshua";
+      security.polkit.enable = true;
+
+      services.gnome.gcr-ssh-agent.enable = false;
+
+      services.greetd = {
+        enable = true;
+        settings.default_session = {
+          command = "niri-session";
+          user = "joshua";
+        };
       };
     };
-  };
 }
