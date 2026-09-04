6d7854b8404d8cca234bb75d10d7af4a8f9a629e
Joshua <joshua@example.com>
2026-09-04 15:19:27 -0400

    feat(host): declare host memory and add a real swapfile

--- modules/hosts.nix
+++ modules/hosts.nix
@@ -1,5 +1,9 @@
 {
-  den.hosts.x86_64-linux.titan.users.joshua = { };
+  den.hosts.x86_64-linux.titan = {
+    users.joshua = { };
+    memory.totalGiB = 32;
+    memory.swapFileGiB = 24;
+  };
   den.hosts.x86_64-linux.wsl = {
     wsl.enable = true;
     users.joshua = { };
